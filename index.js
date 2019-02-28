0a1ae418a48cea85aad17d78681d3b8770965655
Khalil Saqui <influencifer@gmail.com>
2019-02-28 04:23:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase() 
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase('hello'))
+}
+function logWhisper(string) {
+   console.log(string.toLowerCase('HELLO'))
+}
+function sayHiToGrandma(string) {
+  return string.'HELLO'.toLowerCase()
+}
